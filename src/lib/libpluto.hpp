7cdd9041d8ca3a0252c8c8b50a02396d11f38329
Miles Rout <miles.rout@gmail.com>
2013-02-25 02:04:26 +0000

    Added libpluto header.

---
+++ src/lib/libpluto.hpp
@@ -0,0 +1,23 @@
+#include <cstdint>
+
+#include <stdexcept>
+#include <string>
+
+#include <libasteroid.hpp>
+
+namespace galaxy {
+    namespace pluto {
+        /**
+         * vector<u16> link(asteroid)
+         * 
+         * links the object code in the provided asteroids,
+         * producing a vector of unsigned 16-bit integers
+         * representing a DCPU-16 binary.
+         */
+        std::vector<std::uint16_t> link(std::vector<galaxy::asteroid>);
+
+        class link_error : public std::runtime_error {
+            link_error(const std::string& what_str) : runtime_error(what_str) {}
+        };
+    }
+}
